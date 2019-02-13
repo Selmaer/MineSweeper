efdf385aac024748cbd75ce6afa763d38b521545
Вадим <vadimluck@gmail.com>
2019-02-13 20:37:54 +0200

    Initial commit

---
+++ src/sweeper/Flag.java
@@ -0,0 +1,66 @@
+package sweeper;
+
+public class Flag {
+    private Matrix flagMap;
+    private int countOfClosedBoxes;
+
+    void start() {
+        flagMap = new Matrix(Box.CLOSED);
+        countOfClosedBoxes = Ranges.getSize().x * Ranges.getSize().y;
+    }
+
+    Box get (Coord coord) {
+        return flagMap.get(coord);
+    }
+
+    void setOpenedToBox(Coord coord) {
+        flagMap.set (coord, Box.OPENED);
+        countOfClosedBoxes--;
+    }
+
+
+    void setFlagedToBox(Coord coord) {
+        flagMap.set(coord, Box.FLAGED);
+    }
+
+    void setClosedToBox(Coord coord) {
+        flagMap.set(coord, Box.CLOSED);
+    }
+
+    void toggleFlagedToBox(Coord coord) {
+        switch (flagMap.get(coord)) {
+            case FLAGED : setClosedToBox(coord); break;
+            case CLOSED : setFlagedToBox(coord); break;
+        }
+    }
+
+    int getCountOfClosedBoxes() {
+        return countOfClosedBoxes;
+    }
+
+    void seBombedToBox(Coord coord) {
+        flagMap.set(coord, Box.BOMBED);
+    }
+
+    void setOpenedToClosedBombBox(Coord coord) {
+        if (flagMap.get(coord) == Box.CLOSED) {
+            flagMap.set(coord, Box.OPENED);
+        }
+    }
+
+    void setNobombToFlagedSafeBox(Coord coord) {
+        if (flagMap.get(coord) == Box.FLAGED) {
+            flagMap.set(coord, Box.NOBOMB);
+        }
+    }
+
+    int getCountOfFlagedBoxesAround(Coord coord) {
+        int count = 0;
+        for (Coord around : Ranges.getCoordsAround(coord)) {
+            if (flagMap.get(around) == Box.FLAGED) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/sweeper/Matrix.java
@@ -0,0 +1,21 @@
+package sweeper;
+
+class Matrix {
+    private Box [][] matrix;
+
+    Matrix (Box defaultBox) {
+        matrix = new Box[Ranges.getSize().x] [Ranges.getSize().y];
+        for (Coord coord :Ranges.getAllCoords()) {
+            matrix[coord.x][coord.y] = defaultBox;
+        }
+    }
+    Box get (Coord coord) {
+        return matrix[coord.x][coord.y];
+    }
+
+    void set (Coord coord, Box box) {
+        if (Ranges.inRange(coord)) {
+            matrix[coord.x][coord.y] = box;
+        }
+    }
+}
